914f4b4013cfa89142b5144e23e4a693e32edc09
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-08-11 15:46:44 +1000

    Create API Call to get number of Jira issues

--- First-App/src/index.jsx
+++ First-App/src/index.jsx
@@ -1,9 +1,17 @@
-import ForgeUI, { render, ProjectPage, Fragment, Text } from '@forge/ui';
+import ForgeUI, { render, ProjectPage, Fragment, Text, useState } from '@forge/ui';
+import api, { route } from '@forge/api';
+
+const fetchNumberOfIssues = async () => {
+    const responses = await api.asApp().requestJira(route`/rest/api/3/search`);
+    const data = await responses.json();
+    return data.total;
+}
 
 const App = () => {
+    const [numIssues] = useState(async () => await fetchNumberOfIssues());
     return (
         <Fragment>
-            <Text>Hello!</Text>
+            <Text>Number of issues: {numIssues}</Text>
         </Fragment>
     );
 };
