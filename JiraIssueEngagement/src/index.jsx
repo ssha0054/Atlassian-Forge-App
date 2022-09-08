6423ec6a7bbc31238f4b4407d2e5fd2b6ea9257f
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-08 15:41:15 +1000

    Add engagement score as number of comments in Jira card

--- JiraIssueEngagement/src/index.jsx
+++ JiraIssueEngagement/src/index.jsx
@@ -1,9 +1,18 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from '@forge/ui';
+import api, { route } from '@forge/api';
+
+const fetchNumberOfComments = async function(issueKey) {
+  const response = await api.asApp().requestJira(route`/rest/api/3/issue/${issueKey}/comment`);
+  const data = await response.json();
+  return data.total;
+}
 
 const EngagementPanel = () => {
+  const {platformContext: {issueKey}} = useProductContext();
+  const [numComments] = useState(fetchNumberOfComments(issueKey));
   return (
     <Fragment>
-      <Text>Hello world!</Text>
+      <Text>Engagement score: {numComments}</Text>
     </Fragment>
   );
 };
