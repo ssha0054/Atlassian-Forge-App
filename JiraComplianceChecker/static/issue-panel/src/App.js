811860d9b2e0638f42a96aa8623f80cd97ef00c0
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-09 00:14:18 +1000

    Rendering content on issue panel

--- JiraComplianceChecker/static/issue-panel/src/App.js
+++ JiraComplianceChecker/static/issue-panel/src/App.js
@@ -8,9 +8,16 @@ function App() {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
 
+  var issueScoreText = data + "%";
   return (
     <div>
       {data ? data : 'Loading...'}
+      <svg width="100%" height="50px">
+        <g class="bars">
+          <rect fill="#ff0000" width="100%" height="25"></rect>
+          <rect fill="#00ff00" width={issueScoreText} height="25"></rect>
+        </g>
+      </svg>
     </div>
   );
 }
