d14d0a8a1f4b52c2e4bc54a958759c818d62afe5
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-09 00:07:01 +1000

    Building issue panel using Jira API calls

--- JiraComplianceChecker/src/index.js
+++ JiraComplianceChecker/src/index.js
@@ -1,11 +1,27 @@
 import Resolver from '@forge/resolver';
+import api, {route} from '@forge/api';
 
 const resolver = new Resolver();
+const requiredSummary = 50;
+const requiredDescription = 4;
 
-resolver.define('getText', (req) => {
+
+resolver.define('getText', async (req) => {
   console.log(req);
+  console.log(`Issue key ${req.context.extension.issue.key}`)
+  const response = await api.asApp().requestJira(route`/rest/api/3/issue/${req.context.extension.issue.key}`);
+  const data = await response.json();
+  var summaryScore = data.fields.summary.length/requiredSummary;
+  if (summaryScore > 1) {
+    summaryScore = 1;
+  }
+  
+  var descriptionScore = data.fields.description.content.length/requiredDescription;
+  if (descriptionScore > 1) {
+    descriptionScore = 1;
+  }
 
-  return 'Hello, world!';
+  return ((summaryScore+descriptionScore)/2)*100;
 });
 
 export const handler = resolver.getDefinitions();
