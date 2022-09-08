3b9c2e6501bad4de66720562a8b2719a972831f9
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-09 00:34:15 +1000

    Create project page

--- JiraComplianceChecker/src/index.js
+++ JiraComplianceChecker/src/index.js
@@ -5,23 +5,32 @@ const resolver = new Resolver();
 const requiredSummary = 50;
 const requiredDescription = 4;
 
+function getScore(data) {
+  var summaryScore = (data.fields.summary? data.fields.summary.length/requiredSummary : 0);
+  if (summaryScore > 1) {
+    summaryScore = 1;
+  }
+  
+  var descriptionScore = (data.fields.description? data.fields.description.content.length/requiredDescription: 0);
+  if (descriptionScore > 1) {
+    descriptionScore = 1;
+  }
+
+  return ((summaryScore+descriptionScore)/2)*100;
+}
 
 resolver.define('getText', async (req) => {
   console.log(req);
   console.log(`Issue key ${req.context.extension.issue.key}`)
   const response = await api.asApp().requestJira(route`/rest/api/3/issue/${req.context.extension.issue.key}`);
   const data = await response.json();
-  var summaryScore = data.fields.summary.length/requiredSummary;
-  if (summaryScore > 1) {
-    summaryScore = 1;
-  }
-  
-  var descriptionScore = data.fields.description.content.length/requiredDescription;
-  if (descriptionScore > 1) {
-    descriptionScore = 1;
-  }
+  return getScore(data);
+});
 
-  return ((summaryScore+descriptionScore)/2)*100;
+resolver.define('getProjectOverview', async (req) => {
+  console.log(req);
+  //console.log(`Issue key ${req.context.extension.issue.key}`)
+  return "Project overview";
 });
 
 export const handler = resolver.getDefinitions();
