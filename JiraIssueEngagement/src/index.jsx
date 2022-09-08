823a3d8acf2ed962f0d2ecc2da4d6f19d814a6f0
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-08 17:03:35 +1000

    Getting data to display on projectpage

--- JiraIssueEngagement/src/index.jsx
+++ JiraIssueEngagement/src/index.jsx
@@ -1,10 +1,26 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from '@forge/ui';
+import ForgeUI, { ProjectPage, render, Fragment, Text, IssuePanel, useProductContext, useState } from '@forge/ui';
 import api, { route } from '@forge/api';
 
 const fetchNumberOfComments = async function(issueKey) {
   const response = await api.asApp().requestJira(route`/rest/api/3/issue/${issueKey}/comment`);
   const data = await response.json();
   return data.total;
+}
+
+const fetchIssuesWithNumberOfComments = async function(projectKey) {
+  const jql = `project in (${projectKey})`;
+  const response = await api.asApp().requestJira(route`/rest/api/3/search?jql=${jql}`);
+  const data = await response.json();
+
+  let issueWithNumberOfComments = [];
+  for (const issue of data.issues) {
+    const numberOfComments = await fetchNumberOfComments(issue.key);
+    issueWithNumberOfComments.push({
+      "key": issue.key, 
+      "summary": issue.fields.summary, 
+      "numComments": numberOfComments});
+  }
+  return issueWithNumberOfComments;
 }
 
 const EngagementPanel = () => {
@@ -22,3 +38,20 @@ export const panel = render(
     <EngagementPanel />
   </IssuePanel>
 );
+
+export const EngagementOverview = () => {
+  const {platformContext: {projectKey}} = useProductContext();
+  const [issues] = useState(fetchIssuesWithNumberOfComments(projectKey));
+  console.log(JSON.stringify(issues));
+  return (
+    <Fragment>
+        <Text>Overview goes here</Text>
+    </Fragment>
+  )
+}
+
+export const engagementOverview = render(
+  <ProjectPage>
+    <EngagementOverview/>
+  </ProjectPage>
+)
