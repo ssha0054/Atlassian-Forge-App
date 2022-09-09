ea4385a890b422b838b2d14d69066c293c95fb9f
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-09 15:26:26 +1000

    Render content on project page

--- JiraComplianceChecker/static/project-page/src/App.js
+++ JiraComplianceChecker/static/project-page/src/App.js
@@ -10,8 +10,28 @@ function App() {
 
   return (
     <div>
-      {data ? data[0].key : 'Loading...'}
-      
+      <table>
+        <tr>
+          <th>Key</th>
+          <th>Score</th>
+        </tr>
+        {data ? 
+          data.map(issue => (
+            <tr>
+              <td>{issue.key}</td>
+              <td>
+                <svg width="100%" height="50px">
+                  <g class="bars">
+                    <rect fill="#3d5599" width="100%" height="25"></rect>
+                    <rect fill="#cd4d3e" width={issue.score} height="25"></rect>
+                  </g>
+                </svg>
+              </td>
+            </tr>
+          )) 
+          : 'Loading...'
+        }
+      </table>
     </div>
   );
 }
